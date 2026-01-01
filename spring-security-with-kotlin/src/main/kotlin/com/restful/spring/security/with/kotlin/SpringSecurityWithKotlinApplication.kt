205b9655a6d3053f6fe215f570734261fe233c89
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2026-01-01 16:01:05 -0300

    setup: kotlin

---
+++ spring-security-with-kotlin/src/main/kotlin/com/restful/spring/security/with/kotlin/SpringSecurityWithKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.restful.spring.security.with.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityWithKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityWithKotlinApplication>(*args)
+}
